9e86b45719bd63a5a33a45e4bcb059ffef2e5afc
anupama <anupamagadde@yahoo.com>
2020-04-14 20:46:24 -0500

    add resetFormInput to createRiddleForm static function

--- riddles-frontend/src/models/riddle.js
+++ riddles-frontend/src/models/riddle.js
@@ -63,6 +63,7 @@ class Riddle {
                let riddle = new Riddle(data)
                riddle.render()
                resetFormInput()
+             
                
             //   riddles.push(riddle)
             //   render(riddle)
